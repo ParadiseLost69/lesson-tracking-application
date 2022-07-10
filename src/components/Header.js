1cccae0d1227c35fd78336e97105575c3459c00e
Teddy <tedloukas@outlook.com>
2022-07-10 18:03:09 +0900

    Import date-fns

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,18 @@
 import "./Header.css";
+import { format } from "date-fns";
 
 export default function Header() {
+  const currentDate = format(new Date(), "eeee, MMMM dd, yyyy");
   return (
     <div className="header">
       <h1 className="header-title-name">Tracking</h1>
       <div className="header-section-tabs">
-        <ul className="header-section-tabs-list">
-          <li>Calendar</li>
-          <li>Planner</li>
-        </ul>
+        <div className="header-section-tabs-list">
+          <h2 className="section-nav">Calendar</h2>
+          <h2 className="section-nav">Planner</h2>
+        </div>
       </div>
-      <h2 className="header-date">Date</h2>
+      <h2 className="header-date">{currentDate}</h2>
     </div>
   );
 }
---
+++ src/components/Planner.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+export default function Planner() {
+  return (
+    <div className="planner-container">
+      <div className="planner title">
+        <h1>Planner</h1>
+      </div>
+    </div>
+  );
+}
