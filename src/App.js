b346698360bbc110f66f4b04337b1ff8ca650364
Teddy <tedloukas@outlook.com>
2022-07-11 23:27:12 +0900

    Add image upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
+import ReactImageUploading from "react-images-uploading";
 function App() {
+  const [images, setImages] = useState([]);
+  const maxNumber = 69;
+  const onChange = (imageList, addUpdateIndex) => {
+    setImages(imageList);
+  };
   return (
     <div className="App">
+      <ReactImageUploading
+        multiple
+        value={images}
+        onChange={onChange}
+        maxNumber={maxNumber}
+        dataURLKey="data_url"
+      >
+        {({
+          imageList,
+          onImageUpload,
+          onImageRemoveAll,
+          onImageUpdate,
+          onImageRemove,
+          isDragging,
+          dragProps,
+        }) => (
+          // write your building UI
+          <div className="upload__image-wrapper">
+            <button
+              style={isDragging ? { color: "red" } : undefined}
+              onClick={onImageUpload}
+              {...dragProps}
+            >
+              Click or Drop here
+            </button>
+            &nbsp;
+            <button onClick={onImageRemoveAll}>Remove all images</button>
+            {imageList.map((image, index) => (
+              <div key={index} className="image-item">
+                <img src={image["data_url"]} alt="" />
+                <div className="image-item__btn-wrapper">
+                  <button onClick={() => onImageUpdate(index)}>Update</button>
+                  <button onClick={() => onImageRemove(index)}>Remove</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </ReactImageUploading>
       <Header />
     </div>
   );
